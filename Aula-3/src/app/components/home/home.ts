c04932484ea6499ca68c642daa5466497e14098c
Bilius10 <jvdrdo@gmail.com>
2025-06-05 07:53:17 -0300

    continuando aula-3

--- Aula-3/src/app/components/home/home.ts
+++ Aula-3/src/app/components/home/home.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { Header } from '../header/header';
+
 
 
 @Component({
   selector: 'app-home',
-  imports: [Header],
+  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
