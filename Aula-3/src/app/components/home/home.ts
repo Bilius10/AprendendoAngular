7f36d599624cb43a6f158e79f5e9d9ae2699ed29
Bilius10 <jvdrdo@gmail.com>
2025-06-06 07:25:20 -0300

    aula 3 finalizada

--- Aula-3/src/app/components/home/home.ts
+++ Aula-3/src/app/components/home/home.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true, 
+  imports: [FormsModule,CommonModule], 
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrls: ['./home.scss'] 
 })
 export class Home {
+  titulo: string = '';
+  descricao: string = '';
+  dataFinalizacao: string = '';
+  tarefas: { titulo: string; descricao: string; dataFinalizacao: string }[] = [];
 
+  adicionarTarefa() {
+    if (this.titulo && this.descricao && this.dataFinalizacao) {
+      console.log('Adicionando tarefa:', this.titulo, this.descricao, this.dataFinalizacao);
+      const novaTarefa = {
+        titulo: this.titulo,
+        descricao: this.descricao,
+        dataFinalizacao: this.dataFinalizacao
+      };
+      this.tarefas.push(novaTarefa);
+
+      this.titulo = '';
+      this.descricao = '';
+      this.dataFinalizacao = '';
+    }
+  }
+
+  removerTarefa(tarefa: { titulo: string; descricao: string; dataFinalizacao: string }) {
+    const index = this.tarefas.indexOf(tarefa);
+    this.tarefas.splice(index);
+  }
 }
